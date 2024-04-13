63917539f4e228afa163f51e6827efb5a9fa5a9a
Kuingsmile <ma_shiqing@163.com>
2024-04-13 21:53:33 +0800

    :sparkles: Feature(custom): update openai models list

--- src/utils/constant.ts
+++ src/utils/constant.ts
@@ -49,13 +49,12 @@ export const languageMap: IStringKeyMap = {
 export const availableModels: IStringKeyMap = {
   'gpt-3.5': 'gpt-3.5-turbo',
   'gpt-3.5-turbo-1106': 'gpt-3.5-turbo-1106',
-  'gpt-3.5-0613': 'gpt-3.5-turbo-0613',
   'gpt-3.5-16k': 'gpt-3.5-turbo-16k',
   'gpt-3.5-16k-0613': 'gpt-3.5-turbo-16k-0613',
   'text-davinci-003': 'text-davinci-003',
   gtp4: 'gpt-4',
-  'gpt-4-turbo-preview': 'gpt-4-turbo-preview',
-  'gpt-4-1106-preview': 'gpt-4-1106-preview',
+  'gpt-4-turbo': 'gpt-4-turbo',
+  'gpt-4-turo-preview': 'gpt-4-turo-preview',
   'gpt4-0613': 'gpt-4-0613',
   'gpt4-32k': 'gpt-4-32k',
   'gpt4-32k-0613': 'gpt-4-32k-0613'
